01cbfd92f6f0cce0527b3287fdb00c1927901a4c
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-03 11:46:06 -0600

    minor

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "foreman_cloutinit" {
   value = "${data.template_cloudinit_config.foreman.rendered}"
 }
 
+output "gitlab_cloutinit" {
+  value = "${data.template_cloudinit_config.lab.rendered}"
+}
+
 output "base_cloutinit" {
   value = "${data.template_cloudinit_config.base.rendered}"
 }
