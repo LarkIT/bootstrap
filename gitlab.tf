5241b36592fa9871e5d1dea5c445e42a5e1fa313
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-03 13:49:42 -0600

    testin

--- gitlab.tf
+++ gitlab.tf
@@ -1,3 +1,14 @@
+data "template_file" "gitlab-bootstrap" {
+  template = "${file("${path.module}/templates/bootstrap.sh.tpl")}"
+  vars {
+    hostname      = "${var.host_prefix}-gitlab-01.${var.internal_domain_name}"
+    puppet_server = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
+    gitlab_server = "${var.host_prefix}-gitlab-01.${var.internal_domain_name}"
+    puppet_env    = "production"
+    role          = "base"
+  }
+}
+
 data "template_file" "gitlab" {
   template = "${file("${path.module}/templates/gitlab-install.sh.tpl")}"
   vars {
@@ -12,7 +23,7 @@ data "template_cloudinit_config" "gitlab" {
   part {
     filename     = "${var.bootscript_script}"
     content_type = "text/x-shellscript"
-    content      = "${data.template_file.bootstrap.rendered}"
+    content      = "${data.template_file.gitlab-bootstrap.rendered}"
   }
 
   part {
