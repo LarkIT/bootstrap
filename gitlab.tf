11caa08364081aa002ded49039460a4c394f9112
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-03 11:52:19 -0600

    minor

---
+++ gitlab.tf
@@ -0,0 +1,29 @@
+data "template_file" "gitlab" {
+  template = "${file("${path.module}/templates/gitlab-install.sh.tpl")}"
+  vars {
+    hostname      = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
+    puppet_server = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
+    puppet_env    = "production"
+    role          = "gitlab"
+  }
+}
+
+data "template_cloudinit_config" "gitlab" {
+  part {
+    filename     = "${var.bootscript_script}"
+    content_type = "text/x-shellscript"
+    content      = "${data.template_file.bootstrap.rendered}"
+  }
+
+  part {
+    filename = "gitlab-install.sh"
+    content_type = "text/x-shellscript"
+    content = "${data.template_file.gitlab.rendered}"
+  }
+
+  part {
+    filename     = "${var.reboot_script}"
+    content_type = "text/x-shellscript"
+    content      = "${file("${path.module}/templates/reboot.sh")}"
+  }
+}
