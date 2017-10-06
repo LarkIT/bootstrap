5f4b5092811b448024a40b758e97beeb1c1c667f
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-10-06 11:11:41 -0600

    adding pulp.tf

---
+++ pulp.tf
@@ -0,0 +1,23 @@
+data "template_file" "pulp-bootstrap" {
+  template = "${file("${path.module}/templates/bootstrap.sh.tpl")}"
+  vars {
+    hostname      = "${var.host_prefix}-pulp-01.${var.internal_domain_name}"
+    puppet_server = "${var.host_prefix}-foreman-01.${var.internal_domain_name}"
+    puppet_env    = "production"
+    role          = "pulp"
+  }
+}
+
+data "template_cloudinit_config" "pulp" {
+  part {
+    filename     = "${var.bootscript_script}"
+    content_type = "text/x-shellscript"
+    content      = "${data.template_file.pulp-bootstrap.rendered}"
+  }
+
+  part {
+    filename     = "${var.reboot_script}"
+    content_type = "text/x-shellscript"
+    content      = "${file("${path.module}/templates/reboot.sh")}"
+  }
+}
